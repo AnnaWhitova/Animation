297426b462df178c07b9c51ccfc0fa35257000f4
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-07-15 12:48:24 +0300

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Анна Белова on 15.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
